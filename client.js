62adf7c214ed0fc5752b0e29b47551d00075b9c6
Vladimir Vherever <vherever@gmail.com>
2015-01-05 23:59:56 +0200

    checking if the name exist

--- client.js
+++ client.js
@@ -1,13 +1,29 @@
 jQuery(function() {
 	var socket 			= io.connect(),
+		$nickForm		= $('#setNick'),
+		$nickError		= $('#nickError'),
+		$nickBox		= $('#nickname'),
 		$messageForm 	= $('#send-message'),
 		$messageBox 	= $('#message'),
 		$chat 			= $('#chat');
 
+		$nickForm.submit(function(e) {
+			e.preventDefault();
+			socket.emit('new user', $nickBox.val(), function(data) {
+				if(data) {
+					$('#nickWrap').hide();
+					$('#contentWrap').show();
+				} else {
+					$nickError.html('That username is already taken! Please chose another');
+				}
+			});
+			$nickBox.val('');
+		});
+
 	$messageForm.submit(function(e) {
 		e.preventDefault();
 		socket.emit('send message', $messageBox.val());
-		$messageBox.val('');
+		$messageBox.val(''); //clear input form
 	});
 	socket.on('new message', function(data) {
 		$chat.append(data + "<br />");
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express'),
-	app 	= express(),
-	server 	= require('http').createServer(app),
-	io 		= require('socket.io').listen(server);
+var express 	= require('express'),
+	app 		= express(),
+	server 		= require('http').createServer(app),
+	io 			= require('socket.io').listen(server),
+	nicknames 	= [];
 
 server.listen(process.env.PORT || 3000, function() {
 		console.log('Listening on port 3000...');
@@ -13,6 +14,17 @@ app.use(express.static('./'))
 });
 
 io.sockets.on('connection', function(socket) {
+	socket.on('new user', function(data, callback) {
+		if(nicknames.indexOf(data) != -1) {
+			callback(false); 
+		} else {
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+		}
+	});
+
 	socket.on('send message', function(data) {
 		io.sockets.emit('new message', data);
 	});
